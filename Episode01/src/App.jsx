562fafec76ecc6f04391083d9d388f0f13a0b763
Sambhav <sambhav12wakhariya@gmail.com>
2026-02-21 23:55:45 +0530

    episode01: image section ui updated

--- Episode01/src/App.jsx
+++ Episode01/src/App.jsx
@@ -9,6 +9,14 @@ const App = () => {
         <Navbar/>
         <Hero />
       </Container>
+      <div className="layout-lines-container">
+        <div className="left-line" />
+        <div className="right-line" />
+      </div>
+      <div className="hero-image-container">
+        <div className="horizontal-line" />
+        <img src="./hero-ui-v6.webp" className="hero-image"/>
+      </div>
     </div>
   )
 }
