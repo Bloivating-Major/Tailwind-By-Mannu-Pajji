8ca99bd3fa6a42705da7945baaec461cbac14171
Sambhav <sambhav12wakhariya@gmail.com>
2026-02-22 19:55:45 +0530

    episode02: initial commit

---
+++ episode02/src/components/hero.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Hero = () => {
+  return (
+    <div className=''>
+        <h1 className='text-white'>Unleash the power of intuitive finance</h1>
+    </div>
+  )
+}
